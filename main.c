6e2008f939e9629de576b4475de07236e0fd30a8
HyeonMooJeon <38064552+HyeonMooJeon@users.noreply.github.com>
2018-04-11 17:06:54 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,31 +6,32 @@
 2013244103 정병철
 */
 
-int main(){
- int first=0,last=0,result=0;
- char operator=NULL;
- 
- printf("연산자를입력하세요");
- scanf("%c",&operator);
+int main() {
+	int first = 0, last = 0, result = 0;
+	char a;
 
- printf("첫번째피연산자를입력하세요");
- scanf("%d",&first);
- 
- printf("두번째피연산자를입력하세요");
- scanf("%d",&last);
- 
- switch(operator){
- 
-   case+: result=(first+last);
-   ;
-   case-:result=(first-last);
-   ;
-   case*:result=(first*last);
-   ;
-   case/:result=(first/last);
-   ;
-  default:
-   ;
-  
- }
+	printf("연산자를입력하세요");
+	scanf("%c",&a);
+
+	printf("첫번째피연산자를입력하세요");
+	scanf("%d", &first);
+
+	printf("두번째피연산자를입력하세요");
+	scanf("%d", &last);
+
+	switch (a) {
+
+	case'+': result = (first + last);
+		break;
+	case'-':result = (first - last);
+		break;
+	case'*':result = (first*last);
+		break;
+	case'/':result = (first / last);
+		break;
+
+	default:
+		break;
+
+	}
 }
