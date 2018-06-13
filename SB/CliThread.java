142fa474621befc97f719b7588b20c0aa0de1301
DESKTOP-VQO1GHO\Vong <ejrqo963321@gmail.com>
2018-06-14 08:28:48 +0900

    서버부분 수정, README 추가

--- SB/CliThread.java
+++ SB/CliThread.java
@@ -38,7 +38,7 @@ public class CliThread extends Thread
   Socket clientSocket = null;
   PrintWriter out = null;
   BufferedReader in = null;
-  Hashtable<String, String> table = new Hashtable(100); //해쉬테이블 생성
+  Hashtable<String, String> table = new Hashtable(1); //해쉬테이블 생성
 
   public CliThread(Socket socket) {
     this.socket = socket;
@@ -58,49 +58,53 @@ public class CliThread extends Thread
   {
     try
     {
-      OutputStream stream = this.socket.getOutputStream();
+      OutputStream stream = socket.getOutputStream();
 
       mIn = new BufferedReader(
-        new InputStreamReader(this.socket.getInputStream()));
+        new InputStreamReader(socket.getInputStream()));
 
-      mOut = new PrintWriter(this.socket.getOutputStream());
-      in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+      mOut = new PrintWriter(socket.getOutputStream());
+      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-     out = new PrintWriter(this.socket.getOutputStream(), true);
+     out = new PrintWriter(socket.getOutputStream(), true);
       os = this.socket.getOutputStream();
-      osw = new OutputStreamWriter(this.os);
-      bw = new BufferedWriter(this.osw);
-      is = this.socket.getInputStream();
+      osw = new OutputStreamWriter(os);
+      bw = new BufferedWriter(osw);
+      is = socket.getInputStream();
 
-      isr = new InputStreamReader(this.is);
-      br = new BufferedReader(this.isr);
-
+      isr = new InputStreamReader(is);
+      br = new BufferedReader(isr);
+      synchronized(this)
+      {
       while ((ParentTag = mIn.readLine()) != null) //읽어들일데이터가 없을떄까지 동작 
 	      {
 	        
+    	  System.out.println("while문 집입 후 "+ParentTag);
 	
-	        if (ParentTag.contains("/")) // '/'가있는 데이터 즉 아이클라가 보낸 데이터 처리
+	        if (ParentTag.contains(",")) // ','가있는 데이터 즉 아이클라가 보낸 데이터 처리
 		    {
 		          
-		          Sending = ParentTag.split("/"); // 태그명/위도,경도 형태로 들어오는 데이터를  '/'를 기준으로 받은 데이터를 나눔
+		          Sending = ParentTag.split(","); // 태그명,위도/경도 형태로 들어오는 데이터를  ','를 기준으로 받은 데이터를 나눔
 		          Tag = Sending[0];  // 태그를  sending[0]에
 		          Loca = Sending[1];  // 위치정보를 sending[1]에 저장
 		
-		          this.table.put(Sending[0], Sending[1]); //해쉬테이블에 태그 , 위치값을 넣음
+		          table.put(Sending[0], Sending[1]); //해쉬테이블에 태그 , 위치값을 넣음
 		
 		          
-		          Enumeration HT = this.table.keys(); //각각의 객체를 하나씩 처리 
+		          Enumeration HT = table.keys(); //각각의 객체를 하나씩 처리 
 		
 		          System.out.println("while문 -  more element ");
 		          key = HT.nextElement().toString();
 		
-		          System.out.println("▷ key 값 : " + key + "  ▷ value 값 : " + (String)this.table.get(key)); //키값과 밸유값을 출력
-		     }
+		          System.out.println("▷ key 값 : " + key + "  ▷ value 값 : " + table.get(key)); //키값과 밸유값을 출력
+		          
+		    }
 	        else if (ParentTag.equals(key)) //부모클라가 태그를 보내고 아이클라와 태그가 동일할시 작동
 		        {
 		          System.out.println("else if 문 들어옴 ");
 		
-		          while (true) //조건문 돔
+		          while((ParentTag = mIn.readLine()) != null)
+		          //while (true) //조건문 돔
 		          {
 		            System.out.println("서버에서 보낸 데이터 : " + Loca);
 		            inputLine = Loca;
@@ -109,7 +113,7 @@ public class CliThread extends Thread
 		            bw.newLine();
 		            bw.flush();
 		            System.out.println("부모 클라이언트로부터 받은 데이터 : " + ParentTag);
-		            this.sleep(1000); //다른쓰레드로 넘기기위해
+		            this.sleep(10); //다른쓰레드로 넘기기위해
 		          }
 		        }
 	        else
@@ -119,6 +123,7 @@ public class CliThread extends Thread
 	
 	        this.sleep(10); //다른쓰레드로 넘기기위해
 	      } //while문
+      }
     }
     catch (IOException e)
     {
@@ -128,7 +133,7 @@ public class CliThread extends Thread
     {
       try
       {
-        this.socket.close();
+        socket.close();
         System.out.println("▶ 클라이언트 연결이 종료되었습니다.");
       }
       catch (IOException e)
